0c64e8831dd8b5416d706c7d3427c69cca0c2818
Polandent223 <samuel.eliuzeq.223@gmail.com>
2026-02-03 08:36:14 -0400

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,1 +1,13 @@
+const firebaseConfig={
+  apiKey:"AIzaSyAFOyMEo2SgWpcs8WHp2nKCO_ax9-lYZWo",
+  authDomain:"poladent-5fa13.firebaseapp.com",
+  databaseURL:"https://poladent-5fa13-default-rtdb.firebaseio.com",
+  projectId:"poladent-5fa13",
+  storageBucket:"poladent-5fa13.firebasestorage.app",
+  messagingSenderId:"539152567873",
+  appId:"1:539152567873:web:89c67ebb14bd55a77900c0"
+};
 
+firebase.initializeApp(firebaseConfig);
+window.auth = firebase.auth();
+window.db = firebase.database();
